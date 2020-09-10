22e8cf498a51b465e838bfe5ce07b11b2ff4f14c
noaheakin <noaheakin@gmail.com>
2020-09-10 09:28:53 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+array = [0..1000]
+
+def collect_multiples (limit)
+    array = Array.new(limit - 1) {|i| i+=1}
+    array.select {|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples (limit)
+    array = Array.new(limit-=1) {|i| i+=1}
+    multiples = array.select {|n| n % 3 == 0 || n % 5 == 0}
+    multiples.sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_reader :limit
+
+    def initialize (limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        array = Array.new(limit - 1) {|i| i += 1}
+        multiples = array.select {|n| n % 3 == 0 || n % 5 == 0}  
+    end
+
+    def sum_multiples
+        array = Array.new(limit - 1) {|i| i+=1}
+        multiples = array.select {|n| n % 3 == 0 || n % 5 == 0}
+        multiples.sum
+    end
+end
